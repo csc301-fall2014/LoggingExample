a0fc1cd7bed12c59aab3e6606eebdf76a31bd7df
Joey Freund <joey@cs.toronto.edu>
2014-11-12 14:10:47 -0500

    Using Logger objects instead of static methods.

---
+++ src/csc301/loggingExample/Logger.java
@@ -0,0 +1,59 @@
+package csc301.loggingExample;
+
+
+public class Logger {
+
+	public static enum Level {
+		TRACE, DEBUG, INFO, WARN, ERROR, FATAL
+	}
+	
+	
+	//=========================================================================
+	
+	
+	private Level minLevel;
+	
+	
+	public Logger(Level minLevel) {
+		this.minLevel = minLevel;
+	}
+	
+	public Logger() {
+		this(Level.DEBUG);
+	}
+	
+	
+	
+	
+	public void log(Level level, String msg, Object ... args){
+		if(level.ordinal() >= this.minLevel.ordinal()){
+			System.out.println(level.toString() + ": " + String.format(msg, args));
+		}
+	}
+	
+	
+	public void trace(String msg, Object ... args){
+		log(Level.TRACE, msg, args);
+	}
+	
+	public void debug(String msg, Object ... args){
+		log(Level.DEBUG, msg, args);
+	}
+	
+	public void info(String msg, Object ... args){
+		log(Level.INFO, msg, args);
+	}
+	
+	public void warn(String msg, Object ... args){
+		log(Level.WARN, msg, args);
+	}
+	
+	public void error(String msg, Object ... args){
+		log(Level.ERROR, msg, args);
+	}
+	
+	public void fatal(String msg, Object ... args){
+		log(Level.FATAL, msg, args);
+	}
+	
+}
--- src/csc301/loggingExample/Main.java
+++ src/csc301/loggingExample/Main.java
@@ -1,26 +1,44 @@
 package csc301.loggingExample;
 
+import csc301.loggingExample.Logger.Level;
+
 public class Main {
 
 	
-	public static void main(String[] args) {
+	private static Logger logger1 = new Logger(Level.TRACE);
+	private static Logger logger2 = new Logger(Level.DEBUG);
 
+	
+	public static void component1(){
+		logger1.trace("One");
+		logger1.debug("Two");
+		logger1.info("Three");
+	}
+	
+	
+	public static void component2(){
 		for (int i = 0; i < 60; i++) {
-			
+
 			try {
-				
+
 				long start = System.currentTimeMillis();
 				Thread.sleep(1000);
 				long end = System.currentTimeMillis();
-				Logging.debug("Slept for %d milliseconds.", end - start);
-				Logging.trace("** Slept for %d milliseconds.", end - start);
+				logger2.debug("Slept for %d milliseconds.", end - start);
+				logger2.trace("** Slept for %d milliseconds.", end - start);
 			} catch (InterruptedException e) {
 				// Exit the loop, if someone interrupts us from sleeping
 				break;
 			}
-			
 		}
-		
 	}
 
+
+
+	public static void main(String[] args) {
+		component1();
+		component2();
+	}
+
+	
 }
