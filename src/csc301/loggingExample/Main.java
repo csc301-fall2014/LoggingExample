75983b76718fabb6f7e3a6d9155ca4113456f8a9
Joey Freund <joey@cs.toronto.edu>
2014-11-12 13:40:52 -0500

    Adding a static DEBUG flag

--- src/csc301/loggingExample/Main.java
+++ src/csc301/loggingExample/Main.java
@@ -2,6 +2,10 @@ package csc301.loggingExample;
 
 public class Main {
 
+	
+	private static boolean DEBUG = true;
+	
+	
 	public static void main(String[] args) {
 
 		for (int i = 0; i < 60; i++) {
@@ -11,7 +15,10 @@ public class Main {
 				long start = System.currentTimeMillis();
 				Thread.sleep(1000);
 				long end = System.currentTimeMillis();
-				System.out.println(String.format("DEBUG: Slept for %d milliseconds.", end - start));
+				
+				if(DEBUG){
+					System.out.println(String.format("DEBUG: Slept for %d milliseconds.", end - start));
+				}
 				
 			} catch (InterruptedException e) {
 				// Exit the loop, if someone interrupts us from sleeping
