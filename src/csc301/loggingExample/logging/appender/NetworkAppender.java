46e3d569f987e66d466b9bf278cff8ea079f6000
Joey Freund <joey@cs.toronto.edu>
2014-11-13 11:40:01 -0500

    Changing the ConsoleAppender to run in a separate thread.

--- src/csc301/loggingExample/logging/appender/NetworkAppender.java
+++ src/csc301/loggingExample/logging/appender/NetworkAppender.java
@@ -6,13 +6,23 @@ public class NetworkAppender extends AbstractAppender {
 
 	@Override
 	public void append(LogMessage logMessage) {
-		// Let's pretend like we're sending this message to a remote server ...
-		// Instead of actually sending the data, we'll just sleep for 250 ms.
-		try {
-			Thread.sleep(250);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		// We still pretend to send this message to a remote server ...
+		// Only this time we do it in a separate thread.
+		
+		Thread thread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(250);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		});
+		
+		thread.start();
 	}
 
 
